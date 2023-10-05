715e81efbcc233e30ce429270625c815119c37e3
Igor Wojda <igor.wojda@gmail.com>
2023-10-05 11:37:23 +0200

    on push

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -7,7 +7,6 @@ import io.github.typesafegithub.workflows.actions.actions.SetupJavaV3
 import io.github.typesafegithub.workflows.actions.gradle.GradleBuildActionV2
 import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
 import io.github.typesafegithub.workflows.domain.RunnerType.Windows2022
-import io.github.typesafegithub.workflows.domain.triggers.PullRequest
 import io.github.typesafegithub.workflows.domain.triggers.Push
 import io.github.typesafegithub.workflows.dsl.workflow
 import io.github.typesafegithub.workflows.yaml.writeToFile
@@ -15,8 +14,7 @@ import io.github.typesafegithub.workflows.yaml.writeToFile
 workflow(
     name = "Build",
     on = listOf(
-        Push(branches = listOf("main")),
-        PullRequest(),
+        Push(),
     ),
     sourceFile = __FILE__.toPath(),
 ) {
